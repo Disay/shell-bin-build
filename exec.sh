e284f667ecc8282616e959309495a11b10e9b2c7
Disay <wj@8lab.cn>
2020-08-06 17:38:40 +0800

    update exec.sh

--- exec.sh
+++ exec.sh
@@ -9,7 +9,7 @@ workspace=$(cd `dirname $0`; pwd)
 # where to save the installer (parent of directory containing files)
 targetDir=`dirname "${workspace}"`
 execDir=$workspace/exec
-sourceDir=${1:-$workspace/import}
+sourceDir=$workspace/import
 buildDir=$workspace/build
 logDir=$workspace/logs
 dateTag=`date "+%Y%m%d"`
@@ -22,15 +22,13 @@ fi
 
 if [ ! -d $logDir ];then 
     echo "mkdir logs directory "
-    mkdir -p $workspace/logs
 fi 
 
 if [ ! -d "$sourceDir" ]; then
   echo "please put your script to $sourceDir "
-  echo "Usage: $0 [path/to/file]"
-  echo "Example: $0 $sourceDir"
+  echo "Usage: $0 [workspace]"
+  echo "Example: $0 "
   echo "The self-extracting installer a-$dateTag.bin would be created in $execDir"
-  mkdir -p $workspace/import
   exit 1
 fi
 
@@ -43,41 +41,37 @@ fi
 
 if [ ! -d "$execDir/$dateTag" ]; then
     echo "[INFO] Create directory exec$dateTag"  >> $logDir/build-$dateTag.log
-    mkdir -p  $execDir/$dateTag
+    mkdir $execDir/$dateTag
 fi
 # ensure all executable files in the target folder have the x bit set
-
-#chmod +x $sourceDir/*.sh 
+chmod +x $sourceDir/*.sh 
 
 # check for the makeself tool
 script=`ls $sourceDir/`
 for i in $script;
 do 
     echo "It's going to copy $sourceDir/$i to  $buildDir" >> $logDir/build-$dateTag.log
-    mv -f  $sourceDir/$i $buildDir
+    mv $sourceDir/$i $buildDir
     if [ -d $buildDir/$i ];then 
         projectName=`basename "$i"`
 	if [ -f  $buildDir/$i/$i.sh ]; then  
 	echo "[WARNING]The a directory  $i  will be packaged into a installer, when the binary is executed, the $i.sh script is executed" >> $logDir/build-$dateTag.log
 	cd $buildDir/$i
-        #CFLAGs=-static	shc -e 28/01/2023 -r -f ./$projectName.sh -o ./main
-        shc -e 28/01/2023 -r -f ./$projectName.sh -o ./main
-	chmod +x main
+	shc -e 28/01/2023 -r -f ./$projectName.sh -o ./main
         $makeself --follow --nocomp "./" "$buildDir/${projectName}-$dateTag.bin" "buildup $projectName" ./main
 	cd -
         if [ -d $execDir/$dateTag/$i ];then
             rm -rf $execDir/$dateTag/$i
-        fi     
-        mv -f $buildDir/* $execDir/$dateTag
+        fi
+        mv $buildDir/* $execDir/$dateTag
         echo "find your binary in $execDir"
         echo "`date +%Y-%m-%d` `date +%R` build $i " >> $logDir/build-$dateTag.log        
         fi
     else		  
         projectNameVersion=`basename -s .sh "$i"`
-	shc -e 28/01/2023 -r -f $buildDir/$projectNameVersion.sh -o $buildDir/$projectNameVersion-$dateTag.bin
-	chmod +x $buildDir/$projectNameVersion-$dateTag.bin
+	shc -e 28/01/2023 -r -f ./$projectNameVersion.sh -o ./$projectNameVersion-$dateTag.bin
         #$makeself --follow --nocomp "$buildDir" "$buildDir/${projectNameVersion}-$dateTag.bin" "buildup $projectNameVersion" ./$projectNameVersion
-        mv -f $buildDir/* $execDir/$dateTag
+        mv $buildDir/* $execDir/$dateTag
         echo "find your binary in $execDir"
         echo "`date +%Y-%m-%d` `date +%R` build $i " >> $logDir/build-$dateTag.log
     fi
